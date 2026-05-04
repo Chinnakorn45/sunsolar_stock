f17e214ede7379ca771f1b60b2d85dbd467b8200
Chinnakorn45 <chinnakorntongsaard@gmail.com>
2026-05-04 16:26:52 +0700

    test8

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,5 +2,5 @@
 // ดึงค่า URL ของ Backend จาก Environment Variable ของ Vite/Vercel
 // ถ้าไม่มี (เช่น รันในเครื่อง Local) จะใช้ '/api' เป็นค่าเริ่มต้น (ซึ่ง Vite จะจัดการ Proxy ให้)
 export const API_BASE = import.meta.env.MODE === 'production' 
-  ? 'https://sunsolar-stock-4s8r-4op2zqxhh-chinnakorn45s-projects.vercel.app/api' 
+  ? 'https://sunsolar-stock-4s8r.vercel.app/api' 
   : '/api';
